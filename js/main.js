76391403c2da881ce33094f6ceef190b9a63e709
Bunyod3 <bunyodfarmonov37@gmail.com>
2025-10-16 16:22:47 +0500

    countiry-Mamlakat

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const eldark = document.querySelector('.Dark-mode');
+const elnav = document.querySelector('.container-nav-Q');
+eldark.addEventListener('click', function(){
+    document.body.classList.toggle('Dark')
+});
